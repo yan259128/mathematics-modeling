b3315d972d7adc4f8195cb22fedc87dbcf79b3dc
SyakY2WF8FEM^wRt <lzt0000001@gmail.com>
2024-06-29 20:38:36 +0800

    modified:   time.csv

---
+++ mat2d_round.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random
+def mat2d_round(x: np.ndarray, dim: int = 1, eps=1e-6):
+    # 对矩阵按行或列进行四舍五入, 并保证结果的和等于原矩阵的和的四舍五入结果
+    assert dim <= 1 and len(x.shape) == 2, '输入参数的维度'
+    total_sum = x.sum().round(0).astype(int)
+    sum = x.sum(dim, keepdims= True)
+    sum = sum.round(0).astype(int)
+
+    round_x = np.floor(x).astype(int)
+    num_to_fix = sum - round_x.sum(dim, keepdims= True)
+    if total_sum != sum.sum():
+        pos = np.unravel_index(num_to_fix.argmax(), num_to_fix.shape)
+        num_to_fix[pos] += total_sum - sum.sum()
+
+    diff = x - round_x
+    # 避免完全一样的diff
+    diff += eps * np.random.rand(*diff.shape)
+
+    idx = (-diff).argsort(axis=dim)
+    idx = np.take_along_axis(idx, num_to_fix-1, axis=dim)
+    threshold = np.take_along_axis(diff, idx, axis=dim)
+    mask = (diff >= threshold).astype(int)
+    mask *= (num_to_fix != 0).astype(int)
+    ret = round_x + mask
+    assert total_sum == ret.sum(), '四舍五入失败'
+    return ret
--- test.py
+++ test.py
@@ -1,71 +1,51 @@
 from scipy.optimize import linprog
 import numpy as np
+from mat2d_round import mat2d_round
+from bicycle_iter import bicycle_iter
 
-MaxBicycleNum = 1000
+MaxBicycleNum = 1000  # 最大自行车投放数量
 
-Demands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-           1, 0, 86, 120, 75, 122, 92, 129, 60, 105, 97,
-           2, 86, 0, 58, 124, 103, 149, 117, 60, 74, 119,
-           3, 109, 74, 0, 102, 88, 76, 140, 97, 70, 111,
-           4, 120, 81, 79, 0, 72, 58, 128, 75, 115, 129,
-           5, 70, 140, 109, 138, 0, 88, 114, 51, 140, 71,
-           6, 52, 100, 70, 116, 50, 0, 148, 70, 82, 66,
-           7, 88, 80, 91, 100, 64, 69, 0, 59, 114, 51,
-           8, 119, 149, 90, 139, 93, 132, 135, 0, 110, 143,
-           9, 106, 95, 107, 97, 150, 128, 91, 141, 0, 117,
-           10, 106, 138, 86, 116, 116, 112, 95, 146, 136, 0]
+# 每小时的需求矩阵
+DemandsHourly = [0,1,2,3,4,5,6,7,8,9,10,
+                1,0,86,120,75,122,92,129,60,105,97,
+                2,86,0,58,124,103,149,117,60,74,119,
+                3,109,74,0,102,88,76,140,97,70,111,
+                4,120,81,79,0,72,58,128,75,115,129,
+                5,70,140,109,138,0,88,114,51,140,71,
+                6,52,100,70,116,50,0,148,70,82,66,
+                7,88,80,91,100,64,69,0,59,114,51,
+                8,119,149,90,139,93,132,135,0,110,143,
+                9,106,95,107,97,150,128,91,141,0,117,
+                10,106,138,86,116,116,112,95,146,136,0]
 
-Demands = np.array(Demands)
-Demands = Demands.reshape((11, 11))[1:, 1:]
+DemandsHourly = np.array(DemandsHourly)
+DemandsHourly = DemandsHourly.reshape((11, 11))[1:, 1:]  # 去掉行头和列头
 # 每小时的进车上限
-BicycleIn = Demands.sum(0)
+BicycleIn = DemandsHourly.sum(0)
 # 每小时的出车上限
-BicycleOUT = Demands.sum(1)
+BicycleOut = DemandsHourly.sum(1)
 
 # 计算进车的比例
 
 # 按行进行归一化的需求矩阵
-NormalizedDemandsOut = Demands / Demands.sum(1, keepdims=True)
+NormalizedDemandsOut = DemandsHourly / DemandsHourly.sum(1, keepdims=True)
 
 # print(NormalizedDemandsOut.sum(1))
 
-def ride_bicycle(bicycles : np.array, slice_num = 6):
-    remains = bicycles - BicycleOUT
-    remains = remains * (remains > 0).astype(int)
-    print('remains: ', remains)
 
-    out_bicycles = bicycles - remains
-    total_out = out_bicycles.sum()
-    out_bicycles = out_bicycles * NormalizedDemandsOut
-    out_bicycles = out_bicycles.round(0).astype(int)
-    # 检查四舍五入前后的数量是否一致
-    r = total_out - out_bicycles.sum()
-    if r > 0:
-        gaps = Demands/slice_num - out_bicycles # 需求缺口量
-        gaps = np.floor(gaps) * (gaps > 0).astype(int)
-        gaps = gaps.flatten()
-        adj = np.zeros_like(gaps, dtype=int)
-        idx = gaps.argsort()
-        for i in idx:
-            if r <= 0: break
-            if r >= gaps[i]:
-                adj[i] += gaps[i]
-                r = r - gaps[i]
-            else:
-                adj[i] += r
-                r = 0
-        adj = adj.reshape(out_bicycles.shape)
-        print('r=', total_out - out_bicycles.sum(), ', sum adj: ', adj.sum())
-        out_bicycles += adj
-    elif r < 0:
-        adj = np.zeros_like(out_bicycles, dtype=int)
-        adj = adj.flatten()
-        pass # todo
+# InitBicycleNums = MaxBicycleNum / BicycleOut.sum() * BicycleOut
+# InitBicycleNums = InitBicycleNums.round(0).astype(int)
 
-    print('out_bicycles: \n', out_bicycles, out_bicycles.sum())
+# InitBicycleNums = np.zeros(10, dtype=int)
+# InitBicycleNums[7] = 5000
+# InitBicycleNums[8] = 5000
+# InitBicycleNums[9] = 5000
 
-
-InitBicycleNums = MaxBicycleNum / BicycleOUT.sum() * BicycleOUT
-InitBicycleNums = InitBicycleNums.round(0).astype(int)
-print("InitBicycleNums: ", InitBicycleNums*3)
-ride_bicycle(InitBicycleNums)
+InitBicycleNums = np.array([95, 103,  91, 110,  94,  98, 118,  98, 107, 100])
+# InitBicycleNums = np.array([126, 134, 118, 143, 124, 127, 120, 113, 173, 133])
+print("Init Bicycle:\t", InitBicycleNums.sum(), InitBicycleNums)
+# print("BicycleOut:\t", BicycleOut)
+# print("BicycleIn:\t", BicycleIn)
+for epoch in range(0, 18 * 10):
+    print(f"Epoch {epoch} start")
+    InitBicycleNums, _ = bicycle_iter(InitBicycleNums, (DemandsHourly/6).round(0).astype(int))
